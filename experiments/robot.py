90c1fa07ccd6608703c36afdf1f4505165b30b0e
huy-ha <hqh2101@columbia.edu>
2019-11-01 23:15:04 -0400

    setJointMotorControlArray to set target joint state then stepSimulation in sim.py to check for successful action

--- experiments/robot.py
+++ experiments/robot.py
@@ -4,7 +4,7 @@ import time
 
 
 class Robot:
-    def __init__(self, timeStep, pos=None, maxVelocity=5, maxForce=10000, rot=None):
+    def __init__(self, timeStep, pos=None, maxVelocity=0.1, maxForce=10000, rot=None):
         if pos is None:
             pos = np.array([0, 0, 0])
         if rot is None:
@@ -14,10 +14,8 @@ class Robot:
         self._maxForce = maxForce
         self._rot = rot
         self._pos = pos
+        self.target_joint_state = None
 
-        #################################
-        #########SET UP PYBULLET#########
-        #################################
         # Add UR5 robot and mount (visual only)
         self._robot_body_id = p.loadURDF(
             "assets/ur5/ur5.urdf",
@@ -31,20 +29,13 @@ class Robot:
             "assets/ur5/mount.urdf",
             np.add(self._pos, [0, 0, 0.2]),
             p.getQuaternionFromEuler([0, 0, 0]))
-        # for _ in range(200):
-        #     p.stepSimulation()
-        #     time.sleep(0.02)
 
         # Get revolute joint indices of robot (skip fixed joints)
         robot_joint_info = [p.getJointInfo(self._robot_body_id, i) for i in range(
             p.getNumJoints(self._robot_body_id))]
         self._robot_joint_indices = [
             x[0] for x in robot_joint_info if x[2] == p.JOINT_REVOLUTE]
-        # for joint_id_1 in self._robot_joint_indices:
-        #     for joint_id_2 in self._robot_joint_indices:
-        #         if joint_id_1 != joint_id_2:
-        #             p.setCollisionFilterPair(
-        #                 self._robot_body_id, self._robot_body_id, joint_id_1, joint_id_2, 1)
+
         self._robot_joint_lower_limits = [
             x[8] for x in robot_joint_info if x[2] == p.JOINT_REVOLUTE]
         self._robot_joint_upper_limits = [
@@ -54,7 +45,6 @@ class Robot:
         self._robot_home_joint_config = [-np.pi, -np.pi/2,
                                          np.pi/2, -np.pi/2,
                                          -np.pi/2, 0]
-        # self._robot_home_joint_config = [0, 0, 0, 0, 0, 0]
 
         self._palm_body_id = p.loadURDF("assets/palm/palm.urdf",
                                         # p.URDF_USE_SELF_COLLISION,
@@ -73,7 +63,7 @@ class Robot:
         self._robot_tool_joint_idx = 9
         self._robot_tool_tip_joint_idx = 10
         self._robot_tool_offset = [-0.03, 0, -0.01]
-        # TODO how to do a fixed joint
+
         p.createConstraint(self._robot_body_id,
                            self._robot_tool_joint_idx,
                            self._palm_body_id,
@@ -88,44 +78,20 @@ class Robot:
 
     def getConfigs(self):
         # MaxVelocity, TimeStep,Joints
-        return self._maxVelocity, self._timeStep, 6
+        return self._maxVelocity, self._timeStep, len(self.getJoints())
 
-    def applyAction(self, action):
-        current_joint_state = [p.getJointState(self._robot_body_id, i)[
-            0] for i in self._robot_joint_indices]
-        target_joint_state = current_joint_state + action
-        atupper = target_joint_state >= self._robot_joint_upper_limits
-        atlower = target_joint_state <= self._robot_joint_lower_limits
+    def setTargetJointState(self, joint_state):
+        self.target_joint_state = joint_state
         p.setJointMotorControlArray(self._robot_body_id,
                                     self._robot_joint_indices,
                                     p.POSITION_CONTROL,
-                                    target_joint_state,
-                                    positionGains=0.005*np.ones(
+                                    self.target_joint_state,
+                                    positionGains=0.1*self._maxVelocity*np.ones(
                                         len(self._robot_joint_indices)))
-        return not any(atlower) or any(atupper)
 
     def getJoints(self):
         return [p.getJointState(self._robot_body_id, i)[
             0] for i in self._robot_joint_indices]
 
     def reset(self):
-        # Move robot to home joint configuration
-        for joint_id, joint_pos in zip(self._robot_joint_indices, self._robot_home_joint_config):
-            p.resetJointState(self._robot_body_id, joint_id, joint_pos)
-        # TODO do actual matrix multiplication and remove hard coded palm offsets
-        if self._robot_body_id == 2:
-            p.resetBasePositionAndOrientation(self._palm_body_id,
-                                              np.add(self._pos, [
-                                                     -0.5, -0.1, 0.8]),
-                                              p.getQuaternionFromEuler([0, np.pi/2, 0]))
-            # p.resetBasePositionAndOrientation(self._palm_body_id,
-            #                                   np.add(self._pos, [
-            #                                          0.8, 0.25, 0.4]),
-            #                                   p.getQuaternionFromEuler([0, 0, np.pi/2]))
-        elif self._robot_body_id == 5:
-            p.resetBasePositionAndOrientation(self._palm_body_id,
-                                              np.add(self._pos, [
-                                                     0.5, 0.1, 0.8]),
-                                              p.getQuaternionFromEuler([0, np.pi/2, 0]))
-            # p.resetBasePositionAndOrientation(self._palm_body_id,
-            #                                   np.add(self._pos, [-0.8, -0.25, 0.4]), p.getQuaternionFromEuler([0, 0, np.pi/2]))
+        self.setTargetJointState(self._robot_home_joint_config)
